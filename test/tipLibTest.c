5e0db8a0b7b404a01ddfe8b043c67753f2d251ea
Bryan Moffit <moffit@jlab.org>
2015-12-01 23:06:32 -0500

    Add in a test program.

---
+++ test/tipLibTest.c
@@ -0,0 +1,31 @@
+/*
+ * File:
+ *    tipLibTest.c
+ *
+ * Description:
+ *    Test program for the TIpcie Library
+ *
+ *
+ */
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "TIpcieLib.h"
+
+extern int nfadc;
+
+int 
+main(int argc, char *argv[]) 
+{
+
+  tipOpen();
+  
+  tipInit(0,0);
+ CLOSE:
+  tipClose();
+
+  exit(0);
+}
+
